b4ee3d9348465a97327afbef2dd4f31210d953f2
sacastro25-a11y <sacastro.25@est.ucab.edu.ve>
2025-10-09 09:33:17 -0400

    Create Primitivas.h
    -Se añadieron en esta rama un grupo de primitivas creadas el clase para su uso a futuro.

---
+++ Primitivas.h
@@ -0,0 +1,52 @@
+struct Node {
+    int value;
+    Node *next;
+};
+void insertar(Node **p, int value) {
+    Node *nuevoNodo = new Node;
+    nuevoNodo->value = value;
+    nuevoNodo->next = *p;
+    *p = nuevoNodo;
+}
+
+void eliminar(Node **p, int value) {
+    Node *actual = *p;
+    Node *anterior = nullptr;
+
+    while (actual != nullptr) {
+        if (actual->value == value) {
+            if (anterior == nullptr) {
+                *p = actual->next; // Eliminar el primer nodo
+            } else {
+                anterior->next = actual->next;
+            }
+            delete actual;
+            std::cout << "Valor " << value << " eliminado.\n";
+            return;
+        }
+        anterior = actual;
+        actual = actual->next;
+    }
+    std::cout << "Valor " << value << " no encontrado.\n";
+}
+
+bool esta(Node *p, int value) {
+    Node *actual = p;
+    while (actual != nullptr) {
+        if (actual->value == value) {
+            return true;
+        }
+        actual = actual->next;
+    }
+    return false;
+}
+
+int contar(Node *p) {
+    int contador = 0;
+    Node *actual = p;
+    while (actual != nullptr) {
+        contador++;
+        actual = actual->next;
+    }
+    return contador;
+}
